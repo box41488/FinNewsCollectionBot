098296bb500768a7f8dc40ab4823497e232deaad
box41488 <box2442@rambler.ru>
2026-08-14 11:20:55 +0300

    Update financebot.py

--- financebot.py
+++ financebot.py
@@ -10,8 +10,11 @@ import os
 openai_api_key = os.getenv("OPENAI_API_KEY")
 if not openai_api_key:
     raise ValueError("OPENAI_API_KEY is not set in environment variables")
+    server_chan_keys_env = os.getenv("SERVER_CHAN_KEYS")
+if not server_chan_keys_env:
+    raise ValueError("环境变量 SERVER_CHAN_KEYS 未设置，请在Github Actions中设置此变量！")
+server_chan_keys = server_chan_keys_env.split(",")
 openai_client = OpenAI(api_key=openai_api_key, base_url="https://api.deepseek.com/v1")
-
 # RSS源地址列表
 rss_feeds = {
     " 华尔街见闻":{
